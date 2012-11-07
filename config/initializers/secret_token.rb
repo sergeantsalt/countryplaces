540425c01dbf7377b1de4bf2e53c44f36d5b30d5
Alex Nelson <sergeantsalt@gmail.com>
2012-11-07 05:31:59 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Countryplaces::Application.config.secret_token = '747c45c4d530e965fcc7329f0b9d474a0889115dbae786e29b451554b35983f4af3499ef112d52b7088024fa2002f5fb2b1ef803e30664df7f1d7212deae13bc'
