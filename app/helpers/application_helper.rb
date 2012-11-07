0c19e05453ad09409af226ed7e981f8b63880e25
Alex Nelson <sergeantsalt@gmail.com>
2012-11-07 07:06:47 -0600

    Pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+    def full_title(page_title)
+        base_title = "Country Places Inc"
+        if page_title.empty?
+            base_title
+        else
+            "#{base_title} | #{page_title}"
+        end
+    end
+
 end
